64ede769ab22ec6d1815947219edb2fad7ff7339
nick.nickcheng <nick.nickcheng@a3fa1535-7b30-0410-8862-5d2c5c102a8f>
2007-05-18 16:18:16 +0000

    增加个人工具类

---
+++ trunk/BeautifulSoup/nUtils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+def _getURLPrefix(url):
+    ''''''
+    import urllib2
+    params = urllib2.urlparse.urlparse(url)
+    
+    if params[0] == '':
+        return ''
+    
+    result = params[0] + '://' + params[1]
+    
+    li = params[2].rfind('/')
+    if li > 0:
+        result += params[2][:li]
+        
+    return result
+
