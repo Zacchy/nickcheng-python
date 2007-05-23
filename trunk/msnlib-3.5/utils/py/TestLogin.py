52d781bdb226565ce3c7b87f873e98324a7dfd61
nick.nickcheng <nick.nickcheng@a3fa1535-7b30-0410-8862-5d2c5c102a8f>
2007-05-23 09:12:11 +0000

    msnp15_auth方法中将解析xml的方式由正则改为BeautifulSoup

--- trunk/msnlib-3.5/utils/py/TestLogin.py
+++ trunk/msnlib-3.5/utils/py/TestLogin.py
@@ -541,12 +541,13 @@ class msnd:
         what?'''
         import httplib
         import re
-        	
+        from BeautifulSoup import BeautifulStoneSoup
+        
         #
         debug('MSNP15 Auth begin')
         pattern	= '<?xml version="1.0" encoding="UTF-8"?><Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/" xmlns:wsse="http://schemas.xmlsoap.org/ws/2003/06/secext" xmlns:saml="urn:oasis:names:tc:SAML:1.0:assertion" xmlns:wsp="http://schemas.xmlsoap.org/ws/2002/12/policy" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/03/addressing" xmlns:wssc="http://schemas.xmlsoap.org/ws/2004/04/sc" xmlns:wst="http://schemas.xmlsoap.org/ws/2004/04/trust"><Header><ps:AuthInfo xmlns:ps="http://schemas.microsoft.com/Passport/SoapServices/PPCRL" Id="PPAuthInfo"><ps:HostingApp>{7108E71A-9926-4FCB-BCC9-9A9D3F32E423}</ps:HostingApp><ps:BinaryVersion>4</ps:BinaryVersion><ps:UIVersion>1</ps:UIVersion><ps:Cookies></ps:Cookies><ps:RequestParams>AQAAAAIAAABsYwQAAAAzMDg0</ps:RequestParams></ps:AuthInfo><wsse:Security><wsse:UsernameToken Id="user"><wsse:Username>%s</wsse:Username><wsse:Password>%s</wsse:Password></wsse:UsernameToken></wsse:Security></Header><Body><ps:RequestMultipleSecurityTokens xmlns:ps="http://schemas.microsoft.com/Passport/SoapServices/PPCRL" Id="RSTS"><wst:RequestSecurityToken Id="RST0"><wst:RequestType>http://schemas.xmlsoap.org/ws/2004/04/security/trust/Issue</wst:RequestType><wsp:AppliesTo><wsa:EndpointReference><wsa:Address>http://Passport.NET/tb</wsa:Address></wsa:EndpointReference></wsp:AppliesTo></wst:RequestSecurityToken><wst:RequestSecurityToken Id="RST1"><wst:RequestType>http://schemas.xmlsoap.org/ws/2004/04/security/trust/Issue</wst:RequestType><wsp:AppliesTo><wsa:EndpointReference><wsa:Address>messengerclear.live.com</wsa:Address></wsa:EndpointReference></wsp:AppliesTo><wsse:PolicyReference URI="%s"></wsse:PolicyReference></wst:RequestSecurityToken></ps:RequestMultipleSecurityTokens></Body></Envelope>'
         content	= pattern % (self.email, self.pwd, refURI)
-        	
+#        debug('email: ' + self.email)#        debug('pwd: ' + self.pwd)#        debug('refURI: ' + refURI)        	
         #
         h	= httplib.HTTPS('loginnet.passport.com')
         h.putrequest('POST', '/RST.srf')
@@ -568,8 +569,11 @@ class msnd:
         if errcode != 200:
             raise 'AuthError', [911, 'SSL Auth failed']
         	
-        sToken = re.findall(r'"Compact1">([^<]*)', reply)[0]
-        pToken = re.findall(r'wst:BinarySecret>([^<]*)', reply)[-2]
+        soup = BeautifulStoneSoup(reply)
+        sToken = soup.findAll('wst:requestsecuritytokenresponse')[1].find('wsse:binarysecuritytoken').string
+        pToken = soup.findAll('wst:requestsecuritytokenresponse')[1].find('wst:binarysecret').string
+#        sToken = re.findall(r'"Compact1">([^<]*)', reply)[0]
+#        pToken = re.findall(r'wst:BinarySecret>([^<]*)', reply)[-2]
         	
         return (sToken, pToken)
         	
