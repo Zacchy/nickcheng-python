31c5014fa3b5b25daa244367b799d1dd8fa09c23
nick.nickcheng <nick.nickcheng@a3fa1535-7b30-0410-8862-5d2c5c102a8f>
2007-05-24 11:53:54 +0000

    这个版本的这个文件可以获取完整的联系人列表 就是testABService方法

--- trunk/msnlib-3.5/utils/py/TestLogin.py
+++ trunk/msnlib-3.5/utils/py/TestLogin.py
@@ -519,7 +519,7 @@ class msnd:
         #passportid = self.passport_auth(hash)
         	
         # get and use security-token and proof-token
-        securityToken, proofToken = self.msnp15_auth(refURI)
+        securityToken, proofToken, tokenContacts = self.msnp15_auth(refURI)
         blob = self.mbi_encrypt(proofToken, nonce)
         self._send('USR', 'SSO S %s %s' % (securityToken, blob))
 
@@ -533,8 +533,24 @@ class msnd:
         r = self._recv() # MSG Hotmail Hotmail <length>
         l = int(string.split(r[2])[1]) # get the length of MSG command
         r = self._recvmsg(l)
+        
+        self.testABService(tokenContacts)
 
         return 1
+
+    def testABService(self, tC):
+        ''''''
+        import httplib
+                debug('testABService begin')
+        pattern = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"><soap:Header><ABApplicationHeader xmlns="http://www.msn.com/webservices/AddressBook"><ApplicationId>09607671-1C32-421F-A6A6-CBFAA51AB5F4</ApplicationId><IsMigration>false</IsMigration><PartnerScenario>Initial</PartnerScenario></ABApplicationHeader><ABAuthHeader xmlns="http://www.msn.com/webservices/AddressBook"><ManagedGroupRequest>false</ManagedGroupRequest><TicketToken>%s</TicketToken></ABAuthHeader></soap:Header><soap:Body><ABFindAll xmlns="http://www.msn.com/webservices/AddressBook"><abId>00000000-0000-0000-0000-000000000000</abId><abView>Full</abView><deltasOnly>false</deltasOnly><lastChange>0001-01-01T00:00:00.0000000-08:00</lastChange></ABFindAll></soap:Body></soap:Envelope>'
+        content = pattern % (tC)
+        
+        h	= httplib.HTTPS('contacts.msn.com')        h.putrequest('POST', '/abservice/abservice.asmx')        h.putheader('SOAPAction','http://www.msn.com/webservices/AddressBook/ABFindAll')        h.putheader('Content-Type', 'text/xml; charset=utf-8')        h.putheader('Host', 'contacts.msn.com')        h.putheader('Content-Length', str(len(content)))        h.endheaders()        	        debug('SSL Connecting...')        h.send(content)        	        debug('SSL POST & Get the reply')        errcode, errmsg, headers = h.getreply()        reply	= h.file.read()#        soup = BeautifulStoneSoup(reply)
+        
+        print errcode, errmsg
+#        print reply
+        f = file('contacts.txt', 'w')
+        f.write(reply)                
 
     def msnp15_auth(self, refURI):
         '''Something to say....
@@ -545,8 +561,8 @@ class msnd:
         
         #
         debug('MSNP15 Auth begin')
-        pattern	= '<?xml version="1.0" encoding="UTF-8"?><Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/" xmlns:wsse="http://schemas.xmlsoap.org/ws/2003/06/secext" xmlns:saml="urn:oasis:names:tc:SAML:1.0:assertion" xmlns:wsp="http://schemas.xmlsoap.org/ws/2002/12/policy" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/03/addressing" xmlns:wssc="http://schemas.xmlsoap.org/ws/2004/04/sc" xmlns:wst="http://schemas.xmlsoap.org/ws/2004/04/trust"><Header><ps:AuthInfo xmlns:ps="http://schemas.microsoft.com/Passport/SoapServices/PPCRL" Id="PPAuthInfo"><ps:HostingApp>{7108E71A-9926-4FCB-BCC9-9A9D3F32E423}</ps:HostingApp><ps:BinaryVersion>4</ps:BinaryVersion><ps:UIVersion>1</ps:UIVersion><ps:Cookies></ps:Cookies><ps:RequestParams>AQAAAAIAAABsYwQAAAAzMDg0</ps:RequestParams></ps:AuthInfo><wsse:Security><wsse:UsernameToken Id="user"><wsse:Username>%s</wsse:Username><wsse:Password>%s</wsse:Password></wsse:UsernameToken></wsse:Security></Header><Body><ps:RequestMultipleSecurityTokens xmlns:ps="http://schemas.microsoft.com/Passport/SoapServices/PPCRL" Id="RSTS"><wst:RequestSecurityToken Id="RST0"><wst:RequestType>http://schemas.xmlsoap.org/ws/2004/04/security/trust/Issue</wst:RequestType><wsp:AppliesTo><wsa:EndpointReference><wsa:Address>http://Passport.NET/tb</wsa:Address></wsa:EndpointReference></wsp:AppliesTo></wst:RequestSecurityToken><wst:RequestSecurityToken Id="RST1"><wst:RequestType>http://schemas.xmlsoap.org/ws/2004/04/security/trust/Issue</wst:RequestType><wsp:AppliesTo><wsa:EndpointReference><wsa:Address>messengerclear.live.com</wsa:Address></wsa:EndpointReference></wsp:AppliesTo><wsse:PolicyReference URI="%s"></wsse:PolicyReference></wst:RequestSecurityToken></ps:RequestMultipleSecurityTokens></Body></Envelope>'
-        content	= pattern % (self.email, self.pwd, refURI)
+        pattern	= '<?xml version="1.0" encoding="UTF-8"?><Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/" xmlns:wsse="http://schemas.xmlsoap.org/ws/2003/06/secext" xmlns:saml="urn:oasis:names:tc:SAML:1.0:assertion" xmlns:wsp="http://schemas.xmlsoap.org/ws/2002/12/policy" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/03/addressing" xmlns:wssc="http://schemas.xmlsoap.org/ws/2004/04/sc" xmlns:wst="http://schemas.xmlsoap.org/ws/2004/04/trust"><Header><ps:AuthInfo xmlns:ps="http://schemas.microsoft.com/Passport/SoapServices/PPCRL" Id="PPAuthInfo"><ps:HostingApp>{7108E71A-9926-4FCB-BCC9-9A9D3F32E423}</ps:HostingApp><ps:BinaryVersion>4</ps:BinaryVersion><ps:UIVersion>1</ps:UIVersion><ps:Cookies></ps:Cookies><ps:RequestParams>AQAAAAIAAABsYwQAAAAzMDg0</ps:RequestParams></ps:AuthInfo><wsse:Security><wsse:UsernameToken Id="user"><wsse:Username>%s</wsse:Username><wsse:Password>%s</wsse:Password></wsse:UsernameToken></wsse:Security></Header><Body><ps:RequestMultipleSecurityTokens xmlns:ps="http://schemas.microsoft.com/Passport/SoapServices/PPCRL" Id="RSTS"><wst:RequestSecurityToken Id="RST0"><wst:RequestType>http://schemas.xmlsoap.org/ws/2004/04/security/trust/Issue</wst:RequestType><wsp:AppliesTo><wsa:EndpointReference><wsa:Address>http://Passport.NET/tb</wsa:Address></wsa:EndpointReference></wsp:AppliesTo></wst:RequestSecurityToken><wst:RequestSecurityToken Id="RST1"><wst:RequestType>http://schemas.xmlsoap.org/ws/2004/04/security/trust/Issue</wst:RequestType><wsp:AppliesTo><wsa:EndpointReference><wsa:Address>messengerclear.live.com</wsa:Address></wsa:EndpointReference></wsp:AppliesTo><wsse:PolicyReference URI="%s"></wsse:PolicyReference></wst:RequestSecurityToken><wst:RequestSecurityToken Id="RST2"><wst:RequestType>http://schemas.xmlsoap.org/ws/2004/04/security/trust/Issue</wst:RequestType><wsp:AppliesTo><wsa:EndpointReference><wsa:Address>contacts.msn.com</wsa:Address></wsa:EndpointReference></wsp:AppliesTo><wsse:PolicyReference URI="%s"/></wst:RequestSecurityToken></ps:RequestMultipleSecurityTokens></Body></Envelope>'
+        content	= pattern % (self.email, self.pwd, refURI, 'MBI')
 #        debug('email: ' + self.email)#        debug('pwd: ' + self.pwd)#        debug('refURI: ' + refURI)        	
         #
         h	= httplib.HTTPS('loginnet.passport.com')
@@ -565,17 +581,18 @@ class msnd:
         debug('SSL POST & Get the reply')
         errcode, errmsg, headers = h.getreply()
         reply	= h.file.read()
-        	
-        if errcode != 200:
+        soup = BeautifulStoneSoup(reply)        	
+        if errcode != 200 or soup.find('s:fault') != None:
             raise 'AuthError', [911, 'SSL Auth failed']
+        print reply
         	
-        soup = BeautifulStoneSoup(reply)
         sToken = soup.findAll('wst:requestsecuritytokenresponse')[1].find('wsse:binarysecuritytoken').string
         pToken = soup.findAll('wst:requestsecuritytokenresponse')[1].find('wst:binarysecret').string
+        tokenContacts = soup.findAll('wst:requestsecuritytokenresponse')[2].find('wsse:binarysecuritytoken').string
 #        sToken = re.findall(r'"Compact1">([^<]*)', reply)[0]
 #        pToken = re.findall(r'wst:BinarySecret>([^<]*)', reply)[-2]
         	
-        return (sToken, pToken)
+        return (sToken, pToken, tokenContacts)
         	
     def mbi_encrypt(self, pToken, nonce):
         '''abcd
